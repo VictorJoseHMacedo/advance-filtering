9d3430e6a39776d122d802adc9dd9e259cd0c2af
VictorJoseHMacedo <bitojose8@gmail.com>
2023-12-11 08:16:48 -0300

    Filtro - Ecommerce

---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,27 @@
+import { FaShoppingCart } from "react-icons/fa";
+
+import Category from "./Category/Category";
+import Price from "./Price/Price";
+import Colors from "./Colors/Colors";
+
+import "./Sidebar.css";
+
+function Sidebar({ handleChange }) {
+  return (
+    <>
+      <section className="sidebar">
+        <div className="logo-container">
+          <h1>
+            <FaShoppingCart />
+          </h1>
+        </div>
+
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+        <Colors handleChange={handleChange} />
+      </section>
+    </>
+  );
+}
+
+export default Sidebar;
